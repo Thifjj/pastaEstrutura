f9eaff491aefbdfc965c2466be7d24be3e5fe4ac
Thifjj <thifj.fj@gmail.com>
2025-05-05 12:33:24 -0300

    modified

--- trabalhoM2/menu.h
+++ trabalhoM2/menu.h
@@ -2,8 +2,8 @@
 #define MENU_H
 
 #include <iostream>
-#include <cstdlib>  // Para rand() e srand()
-#include <ctime>    // Para time()
+#include <cstdlib> // Para rand() e srand()
+#include <ctime>   // Para time()
 #include "pessoas.h"
 #include "fila.h"
 using namespace std;
@@ -14,11 +14,14 @@ void esperaEnter()
 }
 
 template <typename TIPO>
-int verificaMenor(Tfila<TIPO> fila[], int qntMAX){
+int verificaMenor(Tfila<TIPO> fila[], int qntMAX)
+{
     int menorI = 0;
     menor = fila[0].quantidade;
-    for(int i = 1; i<qntMAX; i++){
-        if(menor < fila[i].quantidade){
+    for (int i = 1; i < qntMAX; i++)
+    {
+        if (menor < fila[i].quantidade)
+        {
             menorI = i;
         }
     }
@@ -31,30 +34,30 @@ void menu(int qntGuiches_Socio, int qntGuiches_Torcedor, int qntP_antes, int qnt
     Tfila<socioTorcedor> GuichesSocio[qntGuiches_Socio];
     torcedor a;
     socioTorcedor b;
-    int qntSocios  = 0;
+    int qntSocios = 0;
     int qntTorcedor = 0;
-    for (int i = qntP_antes; i > 0; i--)
+
+    // Calcula quantos devem ser sócios (5%)
+    int numSocios = qntP_antes * 5 / 100;
+    int numTorcedores = qntP_antes - numSocios;
+
+    // Inicializa o gerador de números aleatórios apenas uma vez
+    srand(time(0));
+
+    // Cria os sócios
+    for (int i = 0; i < numSocios; i++)
     {
+        criaSocioTorcedor(b, qntSocios);
+        entrar_na_fila(GuichesSocio[verificaMenor(GuichesSocio, qntGuiches_Socio)], b);
+        qntSocios++;
+    }
 
-        // Inicializa o gerador de números aleatórios
-        srand(time(0));
-
-        // Gera um número aleatório entre 0 e 99
-        int randomNum = rand() % 100;
-
-        // Se o número for menor que 5 (5% de chance), cria sócio
-        if (randomNum < 5)
-        {
-            criaSocioTorcedor(b,qntSocios);
-            entrar_na_fila(GuichesSocio[verificaMenor(GuichesSocio,qntGuiches_Socio)],b);
-            qntSocios++;
-        }
-        // Senão (95% de chance), cria torcedor normal
-        else
-        {
-            criaTorcedor(a,qntTorcedor);
-            entrar_na_fila(GuichesTorcedor[verificaMenor(GuichesTorcedor, qntGuiches_Torcedor)],a);
-        }
+    // Cria os torcedores normais
+    for (int i = 0; i < numTorcedores; i++)
+    {
+        criaTorcedor(a, qntTorcedor);
+        entrar_na_fila(GuichesTorcedor[verificaMenor(GuichesTorcedor, qntGuiches_Torcedor)], a);
+        qntTorcedor++;
     }
 }
 
